dafc04eda28f05b77cd79589eb9a946e6a884560
Jordach Makaya H.M <4thinkai@gmail.com>
2024-08-30 02:31:40 +0100

    fixing logging-system.controller.spec bug

--- src/logging-system/logging-system.controller.spec.ts
+++ src/logging-system/logging-system.controller.spec.ts
@@ -1,15 +1,20 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { LoggingSystemController } from "./logging-system.controller";
+import { LoggingSystemService } from "./logging-system.service";
 
 describe("LoggingSystemController", () => {
   let controller: LoggingSystemController;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let service: LoggingSystemService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LoggingSystemController],
+      providers: [LoggingSystemService], // Provide the LoggingSystemService here
     }).compile();
 
     controller = module.get<LoggingSystemController>(LoggingSystemController);
+    service = module.get<LoggingSystemService>(LoggingSystemService);
   });
 
   it("should be defined", () => {
